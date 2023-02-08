52f07e3c2d6099615421510d20d67914eea33f16
BrightBlueBird <99009695+BrightBlueBird@users.noreply.github.com>
2023-02-08 10:35:50 +0100

    Initial commit

---
+++ src/test/java/com/example/githubactionv1/GitHubActionv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubactionv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionv1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
